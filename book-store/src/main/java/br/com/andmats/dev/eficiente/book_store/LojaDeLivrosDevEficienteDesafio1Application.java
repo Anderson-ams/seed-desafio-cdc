19fe886fb43bf58d8588741e73afd0ac2e3bda35
Anderson-ams <ams.bns2021@gmail.com>
2024-09-09 22:26:41 -0300

    fist commit

---
+++ book-store/src/main/java/br/com/andmats/dev/eficiente/book_store/LojaDeLivrosDevEficienteDesafio1Application.java
@@ -0,0 +1,13 @@
+package br.com.andmats.dev.eficiente.book_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeLivrosDevEficienteDesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeLivrosDevEficienteDesafio1Application.class, args);
+	}
+
+}
---
+++ book-store/src/test/java/br/com/andmats/dev/eficiente/book_store/LojaDeLivrosDevEficienteDesafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andmats.dev.eficiente.book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeLivrosDevEficienteDesafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
